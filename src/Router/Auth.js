c645fe22f869abe7ad91b228751c317f7cab9741
hjs926 <joonsun926@gmail.com>
2022-06-12 12:57:07 +0900

    Update Auth.js

--- src/Router/Auth.js
+++ src/Router/Auth.js
@@ -1,4 +1,14 @@
-import React, { useState } from "react";
-function Auth() {
-  const [email, setEmail] = useStaet("");
-}
+// import React, { useState } from "react";
+// function Auth() {
+//   const [email, setEmail] = useStaet("");
+//   const [password, setPassword] = useStaet("");
+//   const [isSignup, setIsSignup] = useStaet(true);
+
+//     const signup = () => {
+//         alert(`${email}과 ${password}로 회원가입을 합니다.`)
+//     }
+
+//     const signin = () => {
+//         alert(`${email}과 ${password}로 로그인을 합니다.`)
+//     }
+// }
