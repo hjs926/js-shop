ad9408174343dd85229b11ee60d9cbd92b8ac657
hjs926 <joonsun926@gmail.com>
2022-06-12 17:08:32 +0900

    Update Auth.js

--- src/Router/Auth.js
+++ src/Router/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
-function Auth() {
-  const [email, setEmail] = useStaet("");
-}
+// import React, { useState } from "react";
+// function Auth() {
+//   const [email, setEmail] = useStaet("");
+// }
